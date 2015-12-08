67da0987560d4101c55592fddcb5bb0766f46f16
ryanholland <rholland@AL-REM-rholland.local>
2015-12-08 11:38:37 -0800

    Migration to Lambda
    Migrated to lambda function, added support for multiple accounts.

--- import_tags.py
+++ import_tags.py
@@ -1,45 +1,37 @@
-import boto.ec2, json, requests
-
-
-#replace with Alert Logic API Key
-API_KEY = 'REPLACE_WITH_YOUR_ALERT_LOGIC_API_KEY'
-AWS_ACCESS_KEY = 'REPLACE_WITH_AWS_ACCESS'
-AWS_SECRET_KEY ='REPLACE_WITH_AWS_SECRET'
+from __future__ import print_function
+import boto3, json, requests
+#API_KEY = 'REPLACE_WITH_YOUR_ALERT_LOGIC_API_KEY'
 
 #request API call static Params
 HEADERS = {'content-type': 'application/json'}
 ALERT_LOGIC_API_URL = 'https://publicapi.alertlogic.net/api/tm/v1//protectedhosts/'
 ALERT_LOGIC_LOG_API_URL = 'https://publicapi.alertlogic.net/api/lm/v1/sources/'
-list_of_aws_instance_ids = []
-list_of_instance_tags = []
-number_of_updates = 0
-number_of_failed_updates = 0
-number_of_log_updates = 0
-number_of_failed_log_updates = 0
+
+
 #Alert Logic source tag is a single value, while AWS has K:V based tags
 #change to True if you want to capture both Key and Value, false is just value
-USE_TAG_DELIMITER = False
+USE_TAG_DELIMITER = True
 
 #If set to true this is the char that will seperate the key and value in Alert Logic UI
 #you can customize this char
-DELIMITER = " | "
-
-
+DELIMITER = ":"
 #format AWS tags for Alert Logic consumption and overrite tags on Alert Logic sources
 #use the AWS instance "Name" for Alert Logic source name
-def update_source_name(id, tags):
-    name = tags['Name']
+def update_source_name(id, tags, API_KEY):
+    name = ""
     newTag = ""
     for tag in tags:
-        if tag != 'Name':
+        if tag['Key'] != 'Name':
             if newTag != "":
                 newTag =  newTag + ','
             newTag =  newTag + '{"name":'
             if USE_TAG_DELIMITER:
-                newTag = newTag + '"' + tag + DELIMITER + tags[tag]  #this gets the key and the value
+                newTag = newTag + '"' + tag['Key'] + DELIMITER + tag['Value'] 
             else:
-                newTag = newTag + '"' + tags[tag]
+                newTag = newTag + '"' + tag['Key'] + " " + tag['Value']
             newTag = newTag + '"}'
+        else:
+        	name = str(tag['Value'])
 
     if newTag == "":
         tag_update = json.dumps({"protectedhost": {"name": name}})
@@ -47,39 +39,38 @@ def update_source_name(id, tags):
         jsonNewTag = '{"protectedhost":{"name":"'+str(name)+'","tags": ['+str(newTag)+']}}'
         tag_update = json.loads(jsonNewTag)
         tag_update = json.dumps(tag_update)
-
-
-
     R = requests.post(ALERT_LOGIC_API_URL + str(id), headers=HEADERS, auth=(API_KEY,''),data = tag_update)
-
     return R.status_code
-
-def get_log_source(instance_id):
+    
+def get_log_source(instance_id, API_KEY):
     params = {'search': instance_id}
     R = requests.get(ALERT_LOGIC_LOG_API_URL, params=params, headers=HEADERS, auth=(API_KEY, ''))
     output = R.json()
+    if output is None:
+        print ("Recieved invalid JSON for query to Log API for instance-id: %s \n Requests Output: %s" % (instance_id, R.text))
+        return None, False
     if output['sources']:
         if 'syslog' in output['sources'][0]:
-            print ("instance_id=%s source %s" % (instance_id, output['sources'][0]['syslog']['id']))
             return output['sources'][0]['syslog']['id'], False
         elif 'eventlog' in output['sources'][0]:
-            print ("instance_id=%s source %s" % (instance_id, output['sources'][0]['eventlog']['id']))
             return output['sources'][0]['eventlog']['id'], True
-    return None
+    return None, False
 
-def update_log_source_name(id, tags, isWin):
-    name = tags['Name']
+def update_log_source_name(id, tags, isWin, API_KEY):
+    name = ""
     newTag = ""
     for tag in tags:
-        if tag != 'Name':
+        if tag['Key'] != 'Name':
             if newTag != "":
                 newTag =  newTag + ','
             newTag =  newTag + '{"name":'
             if USE_TAG_DELIMITER:
-                newTag = newTag + '"' + tag + DELIMITER + tags[tag]  #this gets the key and the value
+                newTag = newTag + '"' + tag['Key'] + DELIMITER + tag['Value'] 
             else:
-                newTag = newTag + '"' + tags[tag]
+                newTag = newTag + '"' + tag['Key'] + " " + tag['Value']
             newTag = newTag + '"}'
+        else:
+        	name = str(tag['Value'])
     if not isWin: 
         if newTag == "":
             tag_update = json.dumps({"syslog": {"name": name, "method" : "agent"}})
@@ -89,6 +80,7 @@ def update_log_source_name(id, tags, isWin):
             tag_update = json.dumps(tag_update)
         R = requests.post(ALERT_LOGIC_LOG_API_URL + '/syslog/' + str(id), headers=HEADERS, auth=(API_KEY,''),data = tag_update)
         return R.status_code
+        
     else:
         if newTag == "":
             tag_update = json.dumps({"eventlog": {"name": name}})
@@ -98,54 +90,90 @@ def update_log_source_name(id, tags, isWin):
             tag_update = json.dumps(tag_update)
         R = requests.post(ALERT_LOGIC_LOG_API_URL + '/eventlog/' + str(id), headers=HEADERS, auth=(API_KEY,''),data = tag_update)
         return R.status_code
-#establish the AWS CLI connection and create lists of tags and instance IDs
-conn = boto.ec2.connect_to_region('us-east-1',
-   aws_access_key_id=AWS_ACCESS_KEY,
-   aws_secret_access_key=AWS_SECRET_KEY)
-reservations = conn.get_all_instances()
-for res in reservations:
-    for inst in res.instances:
-        if 'Name' in inst.tags:
-            list_of_aws_instance_ids.append(inst.id)
-            list_of_instance_tags.append(inst.tags)
+        
 
-#call Alert Logic sources API and get sources detail
-params = {'search': 'i-'}
-R = requests.get(ALERT_LOGIC_API_URL, params=params, headers=HEADERS, auth=(API_KEY, ''))
-output = R.json()
+def lambda_handler(event, context):
+	list_of_aws_instance_ids = []
+	list_of_instance_tags = []
+	total_number_of_updates = 0
+	total_number_of_failed_updates = 0
+	total_number_of_log_updates = 0
+	total_number_of_failed_log_updates = 0
+	number_of_updates = 0
+	number_of_failed_updates = 0
+	number_of_log_updates = 0
+	number_of_failed_log_updates = 0
+	client = boto3.client('ec2')
+	regions = client.describe_regions()
+	for region in regions['Regions']:
+		list_of_aws_instance_ids = []
+		list_of_instance_tags = []
+		number_of_updates = 0
+		number_of_failed_updates = 0
+		number_of_log_updates = 0
+		number_of_failed_log_updates = 0
+		client = boto3.client('ec2', region_name=region['RegionName'])
+		reservations = client.describe_instances()
+		print("Importing tags from region: %s" % region['RegionName'])
+		for res in reservations['Reservations']:
+		    for inst in res['Instances']:
+		        if 'Tags' in inst.keys():
+		        	list_of_aws_instance_ids.append(str(inst['InstanceId']))
+		        	list_of_instance_tags.append(inst['Tags'])
+		        	
+		            
+		            
 
-#proccess AWS instances looking for matches in Alert Logic (by instance ID)
-listIndex = 0
-for listIndex in range(len(list_of_aws_instance_ids)):
-    if output["protectedhosts"]:
-        index = 0
-        for index in range(len(output["protectedhosts"])):
+		#call Alert Logic sources API and get sources detail
+		params = {'search': 'i-'}
+        if(len(list_of_aws_instance_ids) == 0 ):   #skip hitting our API if there's nothing in that region
+                print("Skipping region %s as EC2 returned no instances in that region" % (region['RegionName']))
+            else:
+                R = requests.get(ALERT_LOGIC_API_URL, params=params, headers=HEADERS, auth=(event['apikey'], ''))
+                output = R.json()
+    		#proccess AWS instances looking for matches in Alert Logic (by instance ID)
+    		listIndex = 0
+    		for listIndex in range(len(list_of_aws_instance_ids)):
+    		    if output["protectedhosts"]:
+    		        index = 0
+    		        for index in range(len(output["protectedhosts"])):
 
-            if 'ec2_instance_id' in output["protectedhosts"][index]["protectedhost"]["metadata"]:
-                if list_of_aws_instance_ids[listIndex] == output["protectedhosts"][index]["protectedhost"]["metadata"]["ec2_instance_id"]:
-                    tempID = output["protectedhosts"][index]["protectedhost"]["id"]
-                     #get corresponding log sources ID for the instance
-                    logID, isWin = get_log_source(output["protectedhosts"][index]["protectedhost"]["metadata"]["ec2_instance_id"])
-                    # call update tags and store httpcode
-                    httpCode = update_source_name(tempID,list_of_instance_tags[listIndex])
-                    # call update to sources DB for LM
-                    if logID is not None:
-                        httpCodeLog = update_log_source_name(logID, list_of_instance_tags[listIndex], isWin)
-                    if httpCode == 200:
-                        number_of_updates = number_of_updates + 1
-                    if httpCode == 400 or httpCode == 500:
-                        number_of_failed_updates = number_of_failed_updates + 1
-                    if httpCodeLog == 200:
-                        number_of_log_updates = number_of_log_updates + 1
-                    if httpCodeLog == 400 or httpCodeLog == 500:
-                        number_of_failed_updates = number_of_failed_updates + 1
-                    
-                    
-                   
+    		            if 'ec2_instance_id' in output["protectedhosts"][index]["protectedhost"]["metadata"]:
+    		                if list_of_aws_instance_ids[listIndex] == output["protectedhosts"][index]["protectedhost"]["metadata"]["ec2_instance_id"]:
+    		                    tempID = output["protectedhosts"][index]["protectedhost"]["id"]
+    		                     #get corresponding log sources ID for the instance
+    		                    logID, isWin = get_log_source(output["protectedhosts"][index]["protectedhost"]["metadata"]["ec2_instance_id"],event['apikey'])
+    		                    # call update tags and store httpcode
+    		                    httpCode = update_source_name(tempID,list_of_instance_tags[listIndex],event['apikey'])
+    		                    # call update to sources DB for LM
+    		                    if logID is not None:
+    		                        httpCodeLog = update_log_source_name(logID, list_of_instance_tags[listIndex], isWin,event['apikey'])
+    		                    if httpCode == 200:
+    		                        number_of_updates = number_of_updates + 1
+    		                    if httpCode == 400 or httpCode == 500:
+    		                        number_of_failed_updates = number_of_failed_updates + 1
+    		                        print ("Update of Instance: %s using Protected Host ID: %s failed with return code: %s" % (output["protectedhosts"][index]["protectedhost"]["metadata"]["ec2_instance_id"], tempID, str(httpCode)))
+    		                    if httpCodeLog == 200:
+    		                        number_of_log_updates = number_of_log_updates + 1
+    		                    if httpCodeLog == 400 or httpCodeLog == 500:
+    		                        number_of_failed_updates = number_of_failed_updates + 1
+    		                        print ("Update of Instance: %s using Log Source ID: %s failed with return code: %s" % (output["protectedhosts"][index]["protectedhost"]["metadata"]["ec2_instance_id"], logID, str(httpCode)))
+    		                    
+    		                    
+    		                   
 
 
 
-print ("Successfully updated %s record(s)" % number_of_updates)
-print ("Unsuccessfully updated %s record(s)" % number_of_failed_updates)
-print ("Successfully updated %s log sources(s)" % number_of_updates)
-print ("Unsuccessfully updated %s log sources(s)" % number_of_failed_updates)
+    		print ("Successfully updated %s record(s) in region %s" % (number_of_updates,region['RegionName']))
+    		total_number_of_updates += number_of_updates
+    		total_number_of_failed_updates  += number_of_failed_updates 
+    		total_number_of_log_updates += number_of_log_updates 
+    		total_number_of_failed_log_updates += number_of_failed_log_updates
+    		number_of_updates = 0
+    		number_of_failed_updates = 0
+    		number_of_log_updates = 0
+    		number_of_failed_log_updates = 0
+
+	return "Successfully updated " + str(total_number_of_updates ) + " protectedhosts and " + str(total_number_of_log_updates) + " log sources. " + str(total_number_of_failed_updates) + " failed protectedhosts and " + str(total_number_of_failed_log_updates) + " failed log sources"
+
+
